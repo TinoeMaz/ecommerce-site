55405acfeb4feaae0d13c103170ab392f8972469
TinoeMaz <tinoemaz03@gmail.com>
2021-09-22 15:45:24 +0200

    first changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+#Personal Apps 
+    'django.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
